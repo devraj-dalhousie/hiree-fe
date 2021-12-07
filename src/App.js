f18326d69d8c41c2952902a5d53513eb9d00dff8
Devraj Singh <devrajvit@gmail.com>
2021-12-07 14:58:29 -0400

    Started React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hiree front-end..more to come. A thousand steps begins with a single step.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
